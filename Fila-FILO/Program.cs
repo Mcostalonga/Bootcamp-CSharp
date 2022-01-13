63b8d9a7d1acab0dbe4896eb25b2a2891d0e8b99
Mcostalonga <mlcostalonga@gmail.com>
2022-01-13 13:38:13 -0300

    Adicionando um programa usado para empilhar objetos. Método FILO

---
+++ Fila-FILO/Program.cs
@@ -0,0 +1,27 @@
+﻿// Programa usado para empilhar objetos:
+// Conceito Primeiro que entra, último que sai (FILO - First In, Last Out)
+using EstruturaDoPrograma.Exemplos;
+
+namespace EstruturaDoPrograma
+{
+    class EstruturaDoPrograma
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilha("A");
+            s.Empilha("B");
+            s.Empilha("C");
+            s.Empilha("D");
+            s.Empilha("E");
+            s.Empilha("F");
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            // Console.WriteLine(s.Desempilha()); // Fazer com que a exceção surja.
+        }
+    }
+}
