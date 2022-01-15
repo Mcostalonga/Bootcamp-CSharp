6090d083cb942bedf12d04726bda9ce893dfa2b6
Mcostalonga <mlcostalonga@gmail.com>
2022-01-15 11:49:11 -0300

    Subindo o exemplo de como usar Delegate e Multi Cast Delegate.

---
+++ Exemplo-Delegate/Exemplo-Delegate/Models/Calculadora.cs
@@ -0,0 +1,14 @@
+namespace Exemplo_Delegate.Models
+{
+    public class Calculadora
+    {
+        public static void Somar(int x, int y)
+        {
+            Console.WriteLine($"Adição: {x} + {y} = {x + y}");
+        }
+        public static void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"Subtração: {x} + {y} = {x - y}");
+        }
+    }
+}
---
+++ Exemplo-Delegate/Exemplo-Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using Exemplo_Delegate.Models;
+
+namespace Exemplo_Delegate
+{
+    class Program
+    {
+        public delegate void Operacao(int x, int y);
+        public delegate void MultiOperacao(int x, int y);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nDelegate:");
+            //Operacao op_soma = Calculadora.Somar; // Outra maneira de passar o método para o delegate
+            Operacao op_soma = new Operacao(Calculadora.Somar);
+
+            //op_soma(10,10); // Outra maneira de chamar o delegate
+            op_soma.Invoke(10,10);
+
+            Console.WriteLine("\nMulti Cast Delegate:");
+            MultiOperacao op = Calculadora.Somar;
+            op += Calculadora.Subtrair; // += atribui mais um método ao delegate.
+            op.Invoke(10,10); // Ao chamar o multi cast delegate, o mesmo obedece a ordem FIFO - First in, First Out.
+        }
+    }
+}
